6efcf7d8ae66ae83384c39e9710a5aae66ce6b71
Serkan Reis <serkanreis@gmail.com>
2022-08-04 12:40:23 +0300

    Allow the user to add attributes even though the metadata file is missing them

--- components/collections/creation/UploadDetails.tsx
+++ components/collections/creation/UploadDetails.tsx
@@ -85,11 +85,9 @@ export const UploadDetails = ({ onChange }: UploadDetailsProps) => {
     if (event.target.files === null) return toast.error('No files selected.')
     for (let i = 0; i < event.target.files.length; i++) {
       reader = new FileReader()
-      reader.onload = async (e) => {
+      reader.onload = (e) => {
         if (!e.target?.result) return toast.error('Error parsing file.')
         if (!event.target.files) return toast.error('No files selected.')
-        if (!JSON.parse(await event.target.files[i].text()).attributes)
-          return toast.error(`The file with name '${event.target.files[i].name}' doesn't have an attributes list!`)
         const metadataFile = new File([e.target.result], event.target.files[i].name, { type: 'application/json' })
         setMetadataFilesArray((prev) => [...prev, metadataFile])
       }
