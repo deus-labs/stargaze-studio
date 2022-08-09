9a21b0b74fdf0240bc9227a7c5e40585888404b0
Serkan Reis <serkanreis@gmail.com>
2022-08-09 17:40:43 +0300

    Add page number input for previewing assets

--- components/AssetsPreview.tsx
+++ components/AssetsPreview.tsx
@@ -1,5 +1,8 @@
-import { useCallback, useMemo, useState } from 'react'
+import { useCallback, useMemo } from 'react'
 import { getAssetType } from 'utils/getAssetType'
+
+import { NumberInput } from './forms/FormInput'
+import { useNumberInputState } from './forms/FormInput.hooks'
 
 interface AssetsPreviewProps {
   assetFilesArray: File[]
@@ -9,7 +12,13 @@ interface AssetsPreviewProps {
 const ITEM_NUMBER = 12
 
 export const AssetsPreview = ({ assetFilesArray, updateMetadataFileIndex }: AssetsPreviewProps) => {
-  const [page, setPage] = useState(1)
+  const pageState = useNumberInputState({
+    id: 'page',
+    name: 'page',
+    title: '',
+    placeholder: '1',
+    defaultValue: 1,
+  })
 
   const totalPages = useMemo(() => Math.ceil(assetFilesArray.length / ITEM_NUMBER), [assetFilesArray])
 
@@ -39,62 +48,78 @@ export const AssetsPreview = ({ assetFilesArray, updateMetadataFileIndex }: Asse
   }, [assetFilesArray])
 
   const renderImages = useCallback(() => {
-    return assetFilesArray.slice((page - 1) * ITEM_NUMBER, page * ITEM_NUMBER).map((assetSource, index) => (
-      <button
-        key={assetSource.name}
-        className="relative p-0 w-[100px] h-[100px] bg-transparent hover:bg-transparent border-0 btn modal-button"
-        onClick={() => {
-          updateMetadataFileIndex((page - 1) * ITEM_NUMBER + index)
-        }}
-        type="button"
-      >
-        <label
-          className="relative p-0 w-full h-full bg-transparent hover:bg-transparent border-0 btn modal-button"
-          htmlFor="my-modal-4"
+    return assetFilesArray
+      .slice((pageState.value - 1) * ITEM_NUMBER, pageState.value * ITEM_NUMBER)
+      .map((assetSource, index) => (
+        <button
+          key={assetSource.name}
+          className="relative p-0 w-[100px] h-[100px] bg-transparent hover:bg-transparent border-0 btn modal-button"
+          onClick={() => {
+            updateMetadataFileIndex((pageState.value - 1) * ITEM_NUMBER + index)
+          }}
+          type="button"
         >
-          {getAssetType(assetSource.name) === 'audio' && (
-            <div className="flex absolute flex-col items-center mt-4 ml-2">
-              <img key={`audio-${index}`} alt="audio_icon" className="mb-2 ml-1 w-6 h-6 thumbnail" src="/audio.png" />
-              <span className="flex self-center ">{assetSource.name}</span>
-            </div>
-          )}
-          {getAssetType(assetSource.name) === 'video' &&
-            videoPreviewElements.filter((videoPreviewElement) => videoPreviewElement.key === assetSource.name)}
+          <label
+            className="relative p-0 w-full h-full bg-transparent hover:bg-transparent border-0 btn modal-button"
+            htmlFor="my-modal-4"
+          >
+            {getAssetType(assetSource.name) === 'audio' && (
+              <div className="flex absolute flex-col items-center mt-4 ml-2">
+                <img key={`audio-${index}`} alt="audio_icon" className="mb-2 ml-1 w-6 h-6 thumbnail" src="/audio.png" />
+                <span className="flex self-center ">{assetSource.name}</span>
+              </div>
+            )}
+            {getAssetType(assetSource.name) === 'video' &&
+              videoPreviewElements.filter((videoPreviewElement) => videoPreviewElement.key === assetSource.name)}
 
-          {getAssetType(assetSource.name) === 'image' && (
-            <img
-              key={`image-${index}`}
-              alt="asset"
-              className="px-1 my-1 thumbnail"
-              src={URL.createObjectURL(assetSource)}
-            />
-          )}
-        </label>
-      </button>
-    ))
-  }, [page])
+            {getAssetType(assetSource.name) === 'image' && (
+              <img
+                key={`image-${index}`}
+                alt="asset"
+                className="px-1 my-1 thumbnail"
+                src={URL.createObjectURL(assetSource)}
+              />
+            )}
+          </label>
+        </button>
+      ))
+  }, [pageState])
 
   const nextPage = () => {
-    if (totalPages === page) return
-    setPage(page + 1)
+    if (totalPages === pageState.value) return
+    pageState.onChange(pageState.value + 1)
   }
 
   const prevPage = () => {
-    if (page === 1) return
-    setPage(page - 1)
+    if (pageState.value === 1) return
+    pageState.onChange(pageState.value - 1)
   }
 
   return (
     <div className="flex flex-col items-center">
       <div className="mt-2 w-[400px] h-[300px]">{renderImages()}</div>
       <div className="mt-5 btn-group">
-        <button className="text-white bg-plumbus-light btn" onClick={prevPage} type="button">
+        <button className="h-14 text-white bg-plumbus-light btn" onClick={prevPage} type="button">
           «
         </button>
-        <button className="text-white btn" type="button">
-          Page {page}/{totalPages}
+        <button className="h-14 text-white btn" type="button">
+          Page{' '}
+          <NumberInput
+            {...pageState}
+            className="mr-1 mb-2 ml-2 w-12"
+            onChange={(e) => {
+              if (Number(e.target.value) > totalPages) {
+                pageState.onChange(totalPages)
+              } else if (Number(e.target.value) < 1) {
+                pageState.onChange(1)
+              } else {
+                pageState.onChange(Number(e.target.value))
+              }
+            }}
+          />
+          /{totalPages}
         </button>
-        <button className="text-white bg-plumbus-light btn" onClick={nextPage} type="button">
+        <button className="h-14 text-white bg-plumbus-light btn" onClick={nextPage} type="button">
           »
         </button>
       </div>
