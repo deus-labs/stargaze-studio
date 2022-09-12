8a6dbd3901147819ba2cbff5b88c388d581d73e4
name-user1 <eray@deuslabs.fi>
2022-09-12 12:06:36 +0300

    Create Collection page radio buttons changed

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -33,7 +33,7 @@ export const Sidebar = () => {
         <Anchor
           key={href}
           className={clsx(
-            'py-2 px-4 -mx-4 uppercase', // styling
+            'py-2 px-4 -mx-4 uppercase rounded-lg', // styling
             'hover:bg-white/5 transition-colors', // hover styling
             { 'font-bold bg-plumbus hover:bg-plumbus': router.asPath === href }, // active route styling
             // { 'text-gray-500 pointer-events-none': disabled }, // disabled route styling
